f553256c85a579702a276c268f0d74944a939a44
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-01-16 18:54:34 -0600

    added the basic csv read code from python activities

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,22 @@
+#import os module to create a file path and to read a CSV file
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+#check the path
+print(csvpath)
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
 #The total number of months included in the dataset
 #The net total amount of "Profit/Losses" over the entire period
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,22 @@
+#import os module to create a file path and to read a CSV file
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+#check the path
+print(csvpath)
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
 #The total number of votes cast
 #A complete list of candidates who received votes
 #The percentage of votes each candidate won
