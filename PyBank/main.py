7790be9f13aa59091baca3c93e1d9d080713a883
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-01-16 18:24:55 -0600

    Resources and instructions updated

---
+++ PyBank/main.py
@@ -0,0 +1,14 @@
+#The total number of months included in the dataset
+#The net total amount of "Profit/Losses" over the entire period
+#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period
+
+#Print to terminal and export to text file with the results
+#Financial Analysis
+#----------------------
+#Total Months; XX
+#Total: $XXXXX
+#Average Change: $XXXX.XX
+#Greatest Increase in Profits: Mon-Year ($XXXXXX)
+#Greatest Decrease in Profits: Mon-Year ($XXXXXX)
---
+++ PyPoll/main.py
@@ -0,0 +1,18 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+#Print to terminal and export to text file with the results
+#Election Results
+#----------------------
+#Total Votes: XXXXXXXX
+#----------------------
+#Khan: XX.XXX% (XXXXXXXX)
+#Corey: XX.XXX% (XXXXXXX)
+#li:  XX.XXX% (XXXXXXX)
+#O'Tooley:  XX.XXX% (XXXXXXX)
+#----------------------
+#Winner: NAME
+#----------------------
